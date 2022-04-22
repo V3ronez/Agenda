27567a4636dc72c0072e17d60bab012a2bdd17f5
Veronez <62267732+V3ronez@users.noreply.github.com>
2022-04-22 11:26:34 -0300

    Update homeController.js

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,3 +4,13 @@ exports.index = async (req, res,) => {
     const contatos = await Contato.buscaContatos();
     res.render('index', { contatos });
 };
+
+/*
+5 METODOS DE UM CONTROLLER
+
+index -> lista todos os users
+store/create -> cria um novo user
+delete -> deleta um user
+show -> mostra um user
+update -> atualiza um user
+*/
